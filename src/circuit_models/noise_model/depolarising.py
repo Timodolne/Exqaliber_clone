b322815df6849167f72d329c8b1e110b04b6a3db
Joseph Tedds <joseph.tedds@cambridgeconsultants.com>
2022-07-19 12:55:56 +0100

    Added unit testing

--- src/circuit_models/noise_model/depolarising.py
+++ src/circuit_models/noise_model/depolarising.py
@@ -1,4 +1,6 @@
-from circuit_models.noise_model.base import NOISE_MODEL, BaseNoiseModel
+import unittest
+
+from src.circuit_models.noise_model.base import NOISE_MODEL, BaseNoiseModel
 
 class Depolarising(BaseNoiseModel):
     '''Depolarising noise
@@ -16,7 +18,6 @@ class Depolarising(BaseNoiseModel):
     -------
     get_p()
         Get the probability that the state depolarises
-
     '''
 
     def __init__(self, p: float):
@@ -25,9 +26,16 @@ class Depolarising(BaseNoiseModel):
         ----------
         p : float
             Probability that the state depolarises
+        
+        Raises
+        ------
+        ValueError:
+            p must be within [0,1]
         """
 
         super().__init__(NOISE_MODEL.DEPOLARISING)
+        if p < 0 or p > 1:
+            raise ValueError(f"p = {p} is a probability and must be in [0,1]")
         self.__p = p
     
     def get_p(self) -> float:
@@ -40,3 +48,16 @@ class Depolarising(BaseNoiseModel):
         """
 
         return self.__p
+
+class DepolarisingNoiseTestCase(unittest.TestCase):
+    def test_p_bounds(self):
+        invalid_p = (
+            {'case': 'Negative p', 'p': -1},
+            {'case': 'Large p', 'p': 2},
+        )
+        for case in invalid_p:
+            with self.subTest(case['case']):
+                self.assertRaises(ValueError,Depolarising,case['p'])
+        
+        with self.subTest("p within [0,1]"):
+            self.assertEqual(0.5, Depolarising(0.5).get_p(), "Wrong probability loaded")
