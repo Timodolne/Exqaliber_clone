07e09e34d98f5ae9137de5e811d4a76776fa1c04
Camille de Valk <camille.de.valk@capgemini.com>
2023-04-03 15:29:16 +0200

    build(analytical iae): build analytical iterative amplitude estimation (with noise)

--- exqaliber/experiments/notebooks/comparison_with_noise.py
+++ exqaliber/experiments/notebooks/comparison_with_noise.py
@@ -13,220 +13,231 @@
 #     name: python3
 # ---
 
-"""Compare different amplitude estimation algorithms."""
+"""Compare different noisy amplitude estimation algorithms."""
 
 # # Noisy amplitude estimation
 
-# ### Set-up
+# ## Exqaliber AE
+
+from typing import cast
+
+import matplotlib.pyplot as plt
 
 # +
-
 import numpy as np
-from qiskit import QuantumCircuit
-from qiskit.algorithms import AmplitudeEstimation, EstimationProblem
-from qiskit.circuit import EquivalenceLibrary, Instruction
-from qiskit.primitives import Sampler
-from qiskit.quantum_info import Statevector
-from qiskit.transpiler import PassManager
-from qiskit.transpiler.passes import (
-    BasisTranslator,
-    Decompose,
-    UnrollCustomDefinitions,
+from qiskit.algorithms import (
+    IterativeAmplitudeEstimation,
+    IterativeAmplitudeEstimationResult,
 )
-from qiskit_aer import AerSimulator
-from qiskit_aer.noise import LocalNoisePass, depolarizing_error
+from qiskit.algorithms.amplitude_estimators.iae import (
+    _chernoff_confint,
+    _clopper_pearson_confint,
+)
 
-# from exqaliber.exqaliber_amplitude_estimation import (
-#     ExqaliberAmplitudeEstimation,
-# )
-
-
-np.random.seed(1)
-
+from exqaliber.exqaliber_amplitude_estimation import (
+    ExqaliberAmplitudeEstimation,
+)
 
 # +
-class ControlledBernoulliA(QuantumCircuit):
-    """A circuit representing the Bernoulli A operator."""
+EXPERIMENT = {
+    "true_theta": 0.4,
+    "prior_mean": "true_theta",
+    "prior_std": 0.5,
+    "method": "greedy",
+    "zeta": 1e-5,
+}
 
-    def __init__(self, probability):
-        super().__init__(2, name="A")  # circuit on 2 qubits
+ae = ExqaliberAmplitudeEstimation(0.01, 0.01, **EXPERIMENT)
+estimation_problem = None
 
-        theta_p = 2 * np.arcsin(np.sqrt(2 * probability))
-        self.h(0)
-        self.cry(theta_p, 0, 1)
+result = ae.estimate(estimation_problem)
 
-
-class CustomGroverOperator(QuantumCircuit):
-    """A custom Grover Operator."""
-
-    def __init__(self, oracle, A):
-        super().__init__(A.num_qubits, name="Q")
-        self.append(A.inverse().to_gate(), range(A.num_qubits))
-        self.append(oracle.to_gate(), range(A.num_qubits))
-        self.append(A.to_gate(), range(A.num_qubits))
-
-    def to_gate(self, parameter_map=None, label=None):
-        """Create a gate from instruction."""
-        return super().to_gate(parameter_map, label)
-
-    def power(self, power):
-        """Raise the operator to a power."""
-        power_circuit = self.copy_empty_like(name=f"Q^{power}")
-        for i in range(power):
-            power_circuit.append(self, range(self.num_qubits))
-
-        return power_circuit.decompose()
-
-
-def pretty_print_result(result):
-    """Pretty print the result object."""
-    estimation = result.estimation
-    num_oracle_queries = result.num_oracle_queries
-    confidence_interval = result.confidence_interval
-    p_min, p_max = confidence_interval
-
-    message = (
-        f"Estimation: {estimation:.4f}. Confidence interval: ",
-        f"({p_min:.4f}, {p_max:.4f}).\n",
-        f"Oracle queries: {num_oracle_queries}.",
-    )
-
-    return "".join(message)
-
-
-p = 0.2
-A = ControlledBernoulliA(p)
-obj = [1]
-exact_p = Statevector(A).probabilities(qargs=obj)[1]
-
-oracle = QuantumCircuit(A.num_qubits, name="mark")
-oracle.z(obj)
-Q = CustomGroverOperator(oracle, A)
-
-problem = EstimationProblem(
-    state_preparation=A.to_gate(),  # A operator
-    grover_operator=Q,  # Q operator
-    objective_qubits=obj,
+print(f"Executed {len(result.powers)} rounds")
+print(
+    f"Finished with standard deviation of {result.standard_deviation:.6f} "
+    f"and mean {result.estimation:.6f}, "
+    f"(true theta: {EXPERIMENT['true_theta']})."
+)
+print(
+    f"Finished with epsilon {result.epsilon_estimated:.6f} and estimate "
+    f"{result.estimation:.6f}. Target epsilon was {result.epsilon_target}."
 )
 
-print(f"Exact Probability we're trying to estimate: {exact_p:.5f}")
+print("Done.")
 
-alpha = 0.01
-epsilon_target = 1e-4
+
 # -
 
-# ### Adding noise
+# ## Analytical Iterative AE
+
+# In IAE, the schedule changes.
+
+
+class AnalyticalIAE(IterativeAmplitudeEstimation):
+    """Analytically sampling with Iterative AE."""
+
+    def __init__(self, epsilon_target, alpha, *args, **kwargs):
+        super().__init__(epsilon_target, alpha)
+        self._true_theta = kwargs.get("true_theta", np.pi / 2)
+        self._zeta = kwargs.get("zeta", 0)
+
+    def estimate(self, true_theta):
+        """Estimate with analytical sampling."""
+        # initialize memory variables
+
+        # list of powers k: Q^k, (called 'k' in paper)
+        powers = [0]
+        # list of multiplication factors (called 'q' in paper)
+        ratios = []
+        # a priori knowledge of theta / 2 / pi
+        theta_intervals = [[0, 1 / 4]]
+        # a priori knowledge of the confidence interval of the estimate
+        a_intervals = [[0.0, 1.0]]
+        num_oracle_queries = 0
+        num_one_shots = []
+
+        # maximum number of rounds
+        max_rounds = (
+            int(
+                np.log(self._min_ratio * np.pi / 8 / self._epsilon)
+                / np.log(self._min_ratio)
+            )
+            + 1
+        )
+        # initially theta is in the upper half-circle
+        upper_half_circle = True
+
+        num_iterations = 0  # keep track of the number of iterations
+        # number of shots per iteration
+        shots = 0
+        # do while loop, keep in mind that we scaled
+        # theta mod 2pi such that it lies in [0,1]
+        while (
+            theta_intervals[-1][1] - theta_intervals[-1][0]
+            > self._epsilon / np.pi
+        ):
+            num_iterations += 1
+
+            # get the next k
+            k, upper_half_circle = self._find_next_k(
+                powers[-1],
+                upper_half_circle,
+                theta_intervals[-1],  # type: ignore
+                min_ratio=self._min_ratio,
+            )
+
+            # store the variables
+            powers.append(k)
+            ratios.append((2 * powers[-1] + 1) / (2 * powers[-2] + 1))
+
+            # set lambda
+            lamda = 2 * k + 1
+
+            noise = np.exp(-lamda * self._zeta)
+            p = 0.5 * (1 - noise * np.cos(lamda * self._true_theta))
+            measurement_outcome = np.random.binomial(1, p)
+
+            one_counts = measurement_outcome
+            prob = measurement_outcome
+
+            num_one_shots.append(one_counts)
+
+            # track number of Q-oracle calls
+            num_oracle_queries += shots * k
+
+            # if on the previous iterations we have K_{i-1} == K_i,
+            # we sum these samples up
+            j = 1  # number of times we stayed fixed at the same K
+            round_shots = 1
+            round_one_counts = one_counts
+            if num_iterations > 1:
+                while (
+                    powers[num_iterations - j] == powers[num_iterations]
+                    and num_iterations >= j + 1
+                ):
+                    j = j + 1
+                    round_shots += 1
+                    round_one_counts += num_one_shots[-j]
+
+            # compute a_min_i, a_max_i
+            if self._confint_method == "chernoff":
+                a_i_min, a_i_max = _chernoff_confint(
+                    prob, round_shots, max_rounds, self._alpha
+                )
+            else:  # 'beta'
+                a_i_min, a_i_max = _clopper_pearson_confint(
+                    round_one_counts, round_shots, self._alpha / max_rounds
+                )
+
+            # compute theta_min_i, theta_max_i
+            if upper_half_circle:
+                theta_min_i = np.arccos(1 - 2 * a_i_min) / 2 / np.pi
+                theta_max_i = np.arccos(1 - 2 * a_i_max) / 2 / np.pi
+            else:
+                theta_min_i = 1 - np.arccos(1 - 2 * a_i_max) / 2 / np.pi
+                theta_max_i = 1 - np.arccos(1 - 2 * a_i_min) / 2 / np.pi
+
+            # compute theta_u, theta_l of this iteration
+            scaling = 4 * k + 2  # current K_i factor
+            theta_u = (
+                int(scaling * theta_intervals[-1][1]) + theta_max_i
+            ) / scaling
+            theta_l = (
+                int(scaling * theta_intervals[-1][0]) + theta_min_i
+            ) / scaling
+            theta_intervals.append([theta_l, theta_u])
+
+            # compute a_u_i, a_l_i
+            a_u = np.sin(2 * np.pi * theta_u) ** 2
+            a_l = np.sin(2 * np.pi * theta_l) ** 2
+            a_u = cast(float, a_u)
+            a_l = cast(float, a_l)
+            a_intervals.append([a_l, a_u])
+
+        # get the latest confidence interval for the estimate of a
+        confidence_interval = tuple(a_intervals[-1])
+
+        # the final estimate is the mean of the confidence interval
+        estimation = np.mean(confidence_interval)
+
+        result = IterativeAmplitudeEstimationResult()
+        result.alpha = self._alpha
+        # result.post_processing = estimation_problem.post_processing
+        result.num_oracle_queries = num_oracle_queries
+
+        result.estimation = estimation
+        result.epsilon_estimated = (
+            confidence_interval[1] - confidence_interval[0]
+        ) / 2
+        result.confidence_interval = confidence_interval
+
+        # result.estimation_processed =
+        # estimation_problem.post_processing(estimation)
+        # confidence_interval = tuple(
+        #     estimation_problem.post_processing(x)
+        #     for x in confidence_interval
+        # )
+        # result.confidence_interval_processed = confidence_interval
+        result.epsilon_estimated_processed = (
+            confidence_interval[1] - confidence_interval[0]
+        ) / 2
+        result.estimate_intervals = a_intervals
+        result.theta_intervals = theta_intervals
+        result.powers = powers
+        result.ratios = ratios
+
+        return result
+
+
+np.random.seed(0)
+iae = AnalyticalIAE(0.01, 0.01, **EXPERIMENT)
+res = iae.estimate(0.1)
+
+print(res.estimation)
 
 # +
-shots_ae = 1024
-sampler_ae = Sampler(options={"shots": shots_ae})
 
-noise_level = 0.01
+plt.plot(res.powers)
 # -
 
-ae = AmplitudeEstimation(
-    num_eval_qubits=4,
-    sampler=sampler_ae,
-)
-
-qc = ae.construct_circuit(problem)
-qc.decompose("QPE").draw("mpl")
-
-# +
-basis_gates = AerSimulator().configuration().basis_gates
-
-# intialize pass mananger
-# stg_pm = StagedPassManager(stages=['QPE', 'powers', 'controlled'])
-pm = PassManager()
-
-# passes
-decompose_QPE = Decompose(["QPE"])
-pm.append(decompose_QPE)
-
-equiv_lib_cQ = EquivalenceLibrary()
-controlled_Q = Q.control(1)
-basis_gates_cQ = basis_gates[:]
-basis_gates_cQ.append("cQ")
-basis_gates_cQ.append("A")
-
-for inst in pm.run(qc):
-    name = inst.operation.name
-    if "Q^" in name:
-        power = name[name.find("^") + 1 :]
-        power_circuit = controlled_Q.power(int(power))
-        equiv_lib_cQ.add_equivalence(inst.operation, power_circuit.decompose())
-
-unroller_cQ = UnrollCustomDefinitions(equiv_lib_cQ, basis_gates=basis_gates_cQ)
-pm.append(unroller_cQ)
-
-basis_translator_cQ = BasisTranslator(
-    equiv_lib_cQ, target_basis=basis_gates_cQ
-)
-pm.append(basis_translator_cQ)
-
-equiv_lib_A = EquivalenceLibrary()
-
-c_Q = QuantumCircuit(A.num_qubits + 1)
-for inst in Q:
-    c_Q.append(inst.operation.control(1), range(A.num_qubits + 1))
-
-for inst in pm.run(qc):
-    name = inst.operation.name
-    if name == "cQ":
-        equiv_lib_A.add_equivalence(inst.operation, c_Q)
-        break
-
-basis_gates_A = basis_gates[:]
-basis_gates_A.append("A")
-basis_gates_A.append("cA")
-basis_gates_A.append("cmark")
-basis_gates_A.append("cA_dg")
-
-unroller_A = UnrollCustomDefinitions(equiv_lib_A, basis_gates=basis_gates_A)
-pm.append(unroller_A)
-
-basis_translator_A = BasisTranslator(equiv_lib_A, target_basis=basis_gates_A)
-pm.append(basis_translator_A)
-
-# create all-qubit error
-
-
-def noise_func(inst: Instruction, qubits):
-    """Add noise after an oracle call."""
-    if "A" not in inst.name:
-        return None
-    error = depolarizing_error(noise_level, inst.num_qubits)
-    error_qc = QuantumCircuit(inst.num_qubits, inst.num_clbits)
-    # error_qc.append(inst, qargs=range(inst.num_qubits),
-    # cargs=range(inst.num_clbits))
-    error_qc.append(
-        error, qargs=range(inst.num_qubits), cargs=range(inst.num_clbits)
-    )
-    return error  # .to_instruction()
-
-
-noise_pass = LocalNoisePass(noise_func, method="append")
-pm.append(noise_pass)
-# -
-
-pm.run(qc).draw("mpl")
-
-# +
-# quantum sampler
-shots_ae = 1024
-
-sampler_ae = Sampler(options={"shots": shots_ae})
-# -
-
-ae = AmplitudeEstimation(
-    num_eval_qubits=4,
-    sampler=sampler_ae,
-)
-ae_result = ae.estimate(problem)
-# ae_noisy_result = ae.estimate(noisy_problem)
-
-print("Amplitude Estimation")
-print(f"Exact probability: {exact_p:.4f}")
-print(pretty_print_result(ae_result))
-print("Noisy results:")
-# print(pretty_print_result(ae_noisy_result))
+plt.plot(np.array(res.estimate_intervals))
