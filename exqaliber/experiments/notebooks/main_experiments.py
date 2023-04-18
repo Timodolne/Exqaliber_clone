ab148e415f5dafe96fb88dcabf8069232be60242
Camille de Valk <camille.de.valk@capgemini.com>
2023-04-18 13:44:49 +0200

    build(final-experiments): create notebook for final experiments

---
+++ exqaliber/experiments/notebooks/main_experiments.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: exqaliber
+#     language: python
+#     name: python3
+# ---
+
+# # Amplitude Estimation experiments
+
+"""Statistical amplitude estimation."""
